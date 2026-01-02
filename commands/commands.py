8d0f53290f1d1a11877d4ec96909e8c45b058367
bianchi-ed <warezdev098@gmail.com>
2026-01-02 14:41:32 -0300

    Updated Readme.md

--- commands/commands.py
+++ commands/commands.py
@@ -1,6 +1,5 @@
 from twitch.message_handler import commands, PREFIX
 
-
-@commands.add(name="help", aliases=["cmds"], cooldown=5)
+@commands.add(name="commands", aliases=[""], cooldown=5)
 def help_command(ctx):
     ctx.reply(f"@{ctx.display_name} -> Commands: {', '.join(PREFIX + c for c in commands.list())}")
