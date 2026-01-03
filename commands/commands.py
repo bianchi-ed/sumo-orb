e767357badfb2f065d5b654ed75894182f8365a5
bianchi-ed <warezdev098@gmail.com>
2026-01-03 19:10:21 -0300

    Updates index.html with recently implemented commands

--- commands/commands.py
+++ commands/commands.py
@@ -2,4 +2,4 @@ from twitch.message_handler import commands, PREFIX
 
 @commands.add(name="commands", aliases=[""], cooldown=5)
 def help_command(ctx):
-    ctx.reply(f"@{ctx.display_name} -> Commands: {', '.join(PREFIX + c for c in commands.list())}")
+    ctx.reply(f"@{ctx.display_name} -> Commands: {', '.join(PREFIX + c for c in commands.list())}. For a comprehensive list of commands and usage examples visit www.sumo-orb.com")
